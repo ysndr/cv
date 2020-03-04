b20da0fb71c61a564450331a99ad20646104484e
Yannik Sander <me@ysndr.de>
2020-03-04 16:17:06 +0100

    Fix build script (>build)

--- default.nix
+++ default.nix
@@ -1,12 +1,12 @@
 {pkgs ? import (if pin == false then <nixpkgs> else pin) {},
- pin ? ./nixpkgs.nix, 
+ pin ? ./nixpkgs.nix,
  publicdir ? "public",
  pdfout ? "curriculum-vitae-yannik-sander.pdf",
  gifout ? "curriculum-vitae-yannik-sander.gif",
  cvsrc ? "cv.yaml",
  cvtemplate ? "cv.tex",
  ... }:
-with  import pkgs.path { 
+with  import pkgs.path {
   overlays = [ ];
 };
 let
@@ -18,25 +18,24 @@ let
   script = {...} @ args: nur.repos.ysndr.lib.wrap ({
     shell = true;
   } // args);
-  
+
   bin = file: script ({ inherit file; bin = true; name = file.name; });
-  
+
   #;
- 
+
   # --------------- Commands ----------------
- 
+
 
   compile-pdf = script {
     name = "compile-pdf";
     paths = [latex] ++ pandoc-pkgs;
     script = ''
       pandoc ${cvsrc} -o ${publicdir}/${pdfout} --template=${cvtemplate} --pdf-engine=xelatex
-      --
     '';
   };
 
   compile-gif = script {
-    name = "compile-gif"; 
+    name = "compile-gif";
     paths = [imagemagick ghostscript];
     script = ''
       ${compile-pdf}
@@ -46,12 +45,12 @@ let
               -loop 0 \
               ${pdfout} \
               -density 350 \
-              ${gifout}  
+              ${gifout}
     '';
   };
 
   publish = script {
-    name = "publish"; 
+    name = "publish";
     paths = [git];
     script =  ''
       set -ex
@@ -63,14 +62,14 @@ let
       git rebase HEAD master
       git push
       popd
-      git add public 
+      git add public
       git commit -m "(Public) updated gist"
       echo "Done! you can now push your repo"
     '';
   };
 
   latex = texlive.combine {
-    inherit (texlive) scheme-small 
+    inherit (texlive) scheme-small
     footmisc pagecolor etoolbox xcolor tcolorbox lastpage fancyhdr  polyglossia xunicode xltxtra marginnote sectsty hyperref environ trimspaces tex-gyre alegreya
                       ;
   };
@@ -91,9 +90,9 @@ let
   };
 
 in {
-  
-  inherit shell; 
+
+  inherit shell;
   ci = {
     inherit compile-pdf publish;
   };
-} 
+}
